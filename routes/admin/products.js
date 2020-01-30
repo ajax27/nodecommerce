1af96b16a7c174943472b3fe32cab7d82d95b0ac
Shaun Collins <shaun.c16@hotmail.co.uk>
2020-01-30 21:07:03 +0000

    Product Routes

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const { validationResult } = require('express-validator');
+const productRepo = require('../../repos/products');
+const productNewTemplate = require('../../views/admin/products/new');
+const { requireTitle, requirePrice } = require('./validators');
 
 const router = express.Router();
 
 router.get('/admin/products', (req, res) => { });
 
-router.get('/admin/products/new', (req, res) => { });
+router.get('/admin/products/new', (req, res) => {
+  res.send(productNewTemplate({}));
+});
+
+router.post('/admin/products/new', [requireTitle, requirePrice], (req, res) => {
+  const errors = validationResult(req);
+  console.log(req.body);
+
+  res.send('Created');
+});
 
 module.exports = router;
